fddc482ad9ba92235e5ca5c477ba8da525b0853f
GandhiHarsh2003 <23gandhiharsh@gmail.com>
2023-11-04 11:47:30 -0400

    new stuff added

---
+++ frontend/components/image.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <img src="/assests/teacher.jpg" alt="Teacher" />
+    </div>
+  );
+}
+
+export default App;
